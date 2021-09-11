9c450bb11cabc5a441c0093527b7f0cae8e57dfa
Aldo Halim <aldohalim28@gmail.com>
2021-09-12 01:46:50 +0800

    Fix bug on byte array conversion

--- src/main/java/com/cz4031/Record.java
+++ src/main/java/com/cz4031/Record.java
@@ -1,8 +1,6 @@
 package com.cz4031;
 
-import java.io.Serializable;
-
-public class Record implements Serializable {
+public class Record {
 
     private boolean empty;
     private char[] tconst;
@@ -12,6 +10,10 @@ public class Record implements Serializable {
     public Record() {
         tconst = new char[10];
         empty = true;
+    }
+
+    public static Record empty() {
+        return new Record();
     }
 
     public boolean isEmpty() {
@@ -44,5 +46,11 @@ public class Record implements Serializable {
 
     public void setNumVotes(int numVotes) {
         this.numVotes = numVotes;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Empty: %s, tconst: %s, avgRating: %f, numVotes: %d", empty, String.valueOf(tconst).trim(),
+                avgRating, numVotes);
     }
 }
